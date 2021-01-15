1329060a351dd49bcadf5de617e86022a036914f
Duy <10cb4tranbaoduy@gmail.com>
2021-01-15 15:42:51 +0700

    make it better

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
 
 
 // App
-const port = process.env.port || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
