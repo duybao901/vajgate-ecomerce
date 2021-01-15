bc5ba9bc4bbf49ceef49bed902379ce4df6c5e96
Duy <10cb4tranbaoduy@gmail.com>
2021-01-15 19:08:24 +0700

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
     expressFileUpload({
